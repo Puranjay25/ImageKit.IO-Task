ae7005673c5f5382f1995c98cdd214b85de0b58a
puranjay25 <puranjay.makhija@gmail.com>
2020-03-20 19:09:05 +0530

    added success message for registration

--- Task/main/views.py
+++ Task/main/views.py
@@ -37,7 +37,7 @@ def index(request):
 		existed_user_with_email = UserDetail.objects.filter(email=email)
 
 		if existed_user_with_email:
-			messages.error(request, "User with Email {} already exists".format(email))
+			messages.error(request, "User with email {} already exists".format(email))
 			return redirect('main:index')
 
 
@@ -45,7 +45,7 @@ def index(request):
 			registered_count_three = False
 			date_today = date.today()
 			ip_address_record = list(UserDetail.objects.filter(ip_address=ip_address, date_created=date_today))
-			if len(ip_address_record)>=3 and not result['success']:
+			if len(ip_address_record)>3 and not result['success']:
 				registered_count_three = True
 				data={"first_name": first_name, "last_name": last_name,
 				"email": email, "password": password, "confirm_password": confirm_password}
@@ -55,6 +55,7 @@ def index(request):
 			
 			UserDetail.objects.get_or_create(first_name=first_name, last_name=last_name,
 				email=email, password=password, ip_address=ip_address, date_created=date_today)
+			messages.success(request, "User with email {} registered sucessfully.".format(email))
 		except Exception as e:
 			raise e
 
